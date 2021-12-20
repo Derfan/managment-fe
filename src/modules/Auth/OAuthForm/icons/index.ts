e675156ebca30b53164ffd840be5df248981c3ab
Borys Medvediev <medvediev.b@gmail.com>
2021-12-20 18:42:03 +0100

    add icons

---
+++ src/modules/Auth/OAuthForm/icons/index.ts
@@ -0,0 +1,3 @@
+export { default as GoogleIcon } from './google.svg';
+export { default as FacebookIcon } from './facebook.svg';
+export { default as GitHubIcon } from './github.svg';
--- src/modules/Auth/OAuthForm/index.tsx
+++ src/modules/Auth/OAuthForm/index.tsx
@@ -1,15 +1,13 @@
+import { GoogleIcon, FacebookIcon, GitHubIcon } from './icons';
+
+import cn from './OAuthForm.module.css';
+
 export const OAuthForm = () => {
     return (
-        <div>
-            <p>
-                <a href="/facebook">Facebook</a>
-            </p>
-            <p>
-                <a href="/google">Google</a>
-            </p>
-            <p>
-                <a href="/github">github</a>
-            </p>
+        <div className={cn.root}>
+            <a href="/facebook"><FacebookIcon /></a>
+            <a href="/google"><GoogleIcon /></a>
+            <a href="/github"><GitHubIcon /></a>
         </div>
     );
 };
