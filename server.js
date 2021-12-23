14db719f1c75fb44a99a5d2e4c481c7f75feeb12
Borys Medvediev <medvediev.b@gmail.com>
2021-12-23 18:38:22 +0100

    fix routing problem

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
+const path = require('path');
 const express = require('express');
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.static('dist'));
+
+app.get('/*', function(_, res) {
+ res.sendFile(path.join(__dirname, './dist', 'index.html'));
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening at http://localhost:${PORT}`)
