13d8899e685030a29fdbcc015b0bd654ce247394
Borys Medvediev <medvediev.b@gmail.com>
2021-12-23 18:21:58 +0100

    add static server

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.use(express.static('dist'));
+
+app.listen(PORT, () => {
+  console.log(`Example app listening at http://localhost:${PORT}`)
+});
